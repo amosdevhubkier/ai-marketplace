80e1440104c84e13297596445b3b002ee5bcece8
amosdevhubkier <alicemeria5@gmail.com>
2026-02-15 04:40:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 const express = require("express");
-const cors = require("cors");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Test route
-app.get("/test", (req, res) => {
-  res.send("API WORKING");
-});
-
-// Home route
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT, () => {
+  console.log(`Serveur fonctionnant sur le port ${PORT}`);
 });
