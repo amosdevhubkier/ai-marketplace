243edcdd85bbaaf43b953d20c05cf664c30ccbe6
amosdevhubkier <alicemeria5@gmail.com>
2026-02-15 18:51:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.listen(PORT, () => {
   console.log(`Serveur fonctionnant sur le port ${PORT}`);
 });
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+// dossier public
+app.use(express.static(path.join(__dirname, "public")));
+
+// 👉 ajouter ceci
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur fonctionnant sur le port ${PORT}`);
+});
